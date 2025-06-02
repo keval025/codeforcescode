77e86e41bd4ba87941bb4a8bea8308df120aec41
keval025 <kevalhirpara025@gmail.com>
2025-06-02 10:12:46 +0530

    add file

---
+++ Cloudberry_Jam.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+
+    int ans = (3 * n * 4) / (3 * 2);
+    cout << ans << endl;
+  }
+  return 0;
+}
